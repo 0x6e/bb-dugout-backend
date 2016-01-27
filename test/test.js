6a59f252b549553425fcee5c51c5d642c5f5f326
0x6e <0x6e@users.noreply.github.com>
2016-01-27 17:16:07 +0000

    Added Mocha testing for the REST API.

---
+++ test/test.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var supertest = require('supertest');
+var should = require('should');
+
+var server = supertest.agent('http://localhost:8080');
+
+
+describe('Dugout authentication API', function() {
+  
+  it('should autheticate Coach with a valid password', function(done) {
+    
+    server.post('/api/authenticate')
+    .send({name: "Coach", password: "iamnuffle"})
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end( function(error, result) {
+      result.status.should.equal(200);
+      result.body.success.should.equal(true);
+      done();
+    });
+    
+  });
+  
+  it('should NOT autheticate Coach becuase he used an invalid password', function(done) {
+    
+    server.post('/api/authenticate')
+    .send({name: "Coach", password: "notmypassword"})
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end( function(error, result) {
+      result.status.should.equal(200);
+      result.body.success.should.equal(false);
+      done();
+    });
+    
+  });
+  
+});
