3fa60ebd96aaf1820eb834a6b019cbdcb0a8eda4
0x6e <0x6e@users.noreply.github.com>
2016-01-27 20:10:34 +0000

    Added additional authentication test.

--- test/test.js
+++ test/test.js
@@ -6,11 +6,25 @@ var should = require('should');
 var server = supertest.agent('http://localhost:8080');
 
 
-describe('Dugout authentication API', function() {
+describe("Dugout authentication API", function() {
   
-  it('should autheticate Coach with a valid password', function(done) {
+  it("should NOT autheticate notCoach because the user doesn't exist", function(done) {
     
-    server.post('/api/authenticate')
+    server.post("/api/authenticate")
+    .send({name: "notCoach", password: "iamnuffle"})
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end( function(error, result) {
+      result.status.should.equal(200);
+      result.body.success.should.equal(false);
+      done();
+    });
+    
+  });
+
+  it("should autheticate Coach with a valid password", function(done) {
+    
+    server.post("/api/authenticate")
     .send({name: "Coach", password: "iamnuffle"})
     .expect("Content-type", /json/)
     .expect(200)
@@ -22,9 +36,9 @@ describe('Dugout authentication API', function() {
     
   });
   
-  it('should NOT autheticate Coach becuase he used an invalid password', function(done) {
+  it("should NOT autheticate Coach becuase he used an invalid password", function(done) {
     
-    server.post('/api/authenticate')
+    server.post("/api/authenticate")
     .send({name: "Coach", password: "notmypassword"})
     .expect("Content-type", /json/)
     .expect(200)
